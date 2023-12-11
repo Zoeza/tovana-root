875e4e0dc31681146a45c5dd224f5f968bba7ce0
Zoeza <zahrahabitaa@gmail.com>
2023-12-11 17:53:00 +0100

    jj

--- src/report_builder/urls.py
+++ src/report_builder/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<str:action>/home/manager', views.home_manager, name='home-manager'),
     path('<str:action>/report/manager', views.report_manager, name='report-manager'),
 
 ]
