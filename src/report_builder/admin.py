20556edc3aaee1aab9a27b1e412215bc537884f3
Zoeza <zahrahabitaa@gmail.com>
2023-12-11 14:32:14 +0100

    update

--- src/report_builder/admin.py
+++ src/report_builder/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Subject, Template, Department
+admin.site.register(Subject)
+admin.site.register(Template)
+admin.site.register(Department)
 
-# Register your models here.
--- src/report_builder/models.py
+++ src/report_builder/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from add_ons import functions
+
 
 # Create your models here.
+class Subject(models.Model):
+    subject_id = functions.serial_number_generator(10).upper()
+    name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    paternal_ancestry = models.CharField(max_length=255)
+    maternal_ancestry = models.CharField(max_length=255)
+    medical_and_surgical_history_summary = models.TextField(max_length=600, default='Not reported.')
+    family_history = models.TextField(max_length=600, default='Not reported.')
+    csv_file = models.FileField(upload_to='subject/csv_file/', null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Template(models.Model):
+    template_name = models.CharField(max_length=100, default='Nutrition_Fitness_Wellness')
+    template = models.FileField(upload_to='templates/', null=True)
+    last_modified = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.template_name
+
+
+class Department(models.Model):
+    health_care_provider = models.CharField(max_length=500, blank=True, null=True)
+    logo = models.ImageField(upload_to='logo/', null=True)
+
+    def __str__(self):
+        return self.health_care_provider
