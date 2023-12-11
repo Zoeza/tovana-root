a4a3026c5316e4728c46307a810f04c8d0b65208
Zoeza <zahrahabitaa@gmail.com>
2023-12-11 17:49:02 +0100

    Update views.py

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -32,6 +32,7 @@ def report_manager(request, action):
     departments_list = Department.objects.all()
     subjects_list = Subject.objects.all()
     reports_list = GeneratedReport.objects.all()
+    url = direction + "/report_builder/reports/list.html"
 
     if action == 'report-builder':
         url = direction + "/report_builder/reports/list.html"
