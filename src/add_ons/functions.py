3141ef6ba56f3e8f89315ce67d2a71f8506528f5
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 14:48:41 +0100

    Update functions.py

--- src/add_ons/functions.py
+++ src/add_ons/functions.py
@@ -136,7 +136,4 @@ def docx_to_pdf(doc_path, path):
     return doc_path
 
 
-if __name__ == "__main__":
-    doc_path = "/tovana-root/site/public/media/reports/RJ85CBCAJP_Nutrition_Fitness_Wellness.docx"
-    path = "/tovana-root/site/public/media/reports/"
-    docx_to_pdf(doc_path, path)
+
