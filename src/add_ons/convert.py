afa0e9867f67ffb38f4dc38c711968fa8b4bc3bf
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 16:00:35 +0100

    Update convert.py

--- src/add_ons/convert.py
+++ src/add_ons/convert.py
@@ -1,7 +1,5 @@
-import subprocess
-import sys
-
 def docx_to_pdf(doc_path, path):
+    import subprocess
     subprocess.call(['/usr/bin/soffice',
                      # '--headless',
                      '--convert-to',
@@ -9,9 +7,6 @@ def docx_to_pdf(doc_path, path):
                      '--outdir',
                      path,
                      doc_path])
-    return doc_path
 
 if __name__ == "__main__":
-    doc_path = "/tovana-root/site/public/media/reports/3OFJ7PXY5U_Wellness.docx"
-    path = "/tovana-root/site/public/media/reports/"
     docx_to_pdf(doc_path, path)
