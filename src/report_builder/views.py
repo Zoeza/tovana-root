2d64a677bbd27041a016bc22a4e53b3be566ded6
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 09:09:52 +0100

    Update views.py

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -139,7 +139,7 @@ def report_manager(request, action):
         if request.method == 'POST':
             report_id = request.POST.get('report_id', False)
             selected_report = GeneratedReport.objects.all().get(id=report_id)
-            pdf_file_path = '/tovana-root/src/templates/RJ85CBCAJP_Nutrition_Fitness_Wellness.docx'
+            pdf_file_path = '/tovana-root/src/templates/RJ85CBCAJP_Nutrition_Fitness_Wellness.pdf'
             return FileResponse(open(pdf_file_path, 'rb'), content_type='application/pdf')
 
 
