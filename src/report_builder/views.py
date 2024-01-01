c67d5cd3747cded11f731115c424531072888cfb
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 15:14:15 +0100

    gg

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -116,7 +116,7 @@ def report_manager(request, action):
             nutrition_report = GeneratedReport()
             nutrition_report.report.save(subject.subject_id + ' ' + 'Nutrition_Fitness_Wellness.docx', report)
             nutrition_report.report_name = subject.subject_id + ' ' + 'Nutrition_Fitness_Wellness'
-            functions.docx_to_pdf('"' + nutrition_report.report.path + '"', "/tovana-root/site/public/media/reports/")
+            functions.docx_to_pdf('"' + nutrition_report.report.path + '"', "/tovana-root/site/public/media/")
 
             nutrition_report.subject = subject.name
             nutrition_report.created = created_at
