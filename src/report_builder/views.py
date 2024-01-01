6f2f93c02b11a32e55f9fefcbf8ca2551f1afa7b
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 13:35:51 +0100

    ll

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -139,5 +139,5 @@ def report_manager(request, action):
         if request.method == 'POST':
             report_id = request.POST.get('report_id', False)
             selected_report = GeneratedReport.objects.all().get(id=report_id)
-            pdf_file_path = "{{selected_report.report.name}}.pdf"
+            pdf_file_path = 'RJ85CBCAJP_Nutrition_Fitness_Wellness.pdf'
             return FileResponse(open(pdf_file_path, 'rb'), content_type='application/pdf')
