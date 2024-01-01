8403dc0c5fec3976714e45054a8ef9dc6b5efd38
Zoeza <zahrahabitaa@gmail.com>
2024-01-01 02:10:48 +0100

    Update views.py

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -135,10 +135,7 @@ def report_manager(request, action):
 
     if action == 'view_report':
         if request.method == 'POST':
-            pdf_file_path = '/tovana-root/src/templates/nutrition_report.pdf'
-            with open(pdf_file_path, 'rb') as pdf_file:
-                # Use FileResponse to serve the PDF file
-                response = FileResponse(pdf_file, content_type='application/pdf')
-                response['Content-Disposition'] = 'inline;filename=file.pdf'
-                return response
+            report_id = request.POST.get('report_id', False)
+            selected_report = GeneratedReport.objects.all().get(id=report_id)
+            return FileResponse(selected_report.report, as_attachment=True)
 
