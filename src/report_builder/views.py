624614952f4ad9bb7f8953a911dde80d608f772a
Zoeza <zahrahabitaa@gmail.com>
2024-01-06 00:36:15 +0100

    Update views.py

--- src/report_builder/views.py
+++ src/report_builder/views.py
@@ -295,14 +295,14 @@ def report_manager(request, action):
         if request.method == 'POST':
             report_id = request.POST.get('report_id', False)
             selected_report = GeneratedReport.objects.all().get(id=report_id)
-            url = "/tovana-root/src/templates/RJ85CBCAJP_Nutrition_Fitness_Wellness.docx"
+            url = "/tovana-root/src/templates/sample.docx"
             doc_file = open(url, "rb")
             html_file = open('/tovana-root/src/templates/output.html', 'wb')
             result = mammoth.convert_to_html(doc_file)
             html_file.write(result.value.encode('utf8'))
             doc_file.close()
             html_file.close()
-            return FileResponse("good")
+            return FileResponse(open('/tovana-root/src/templates/output.html', 'rb'), content_type='application/pdf')
 
             # url = "RJ85CBCAJP_Nutrition_Fitness_Wellness.html"
             # rendered_string = render_to_string(url, {})
